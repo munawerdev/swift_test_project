001070ec901521dedb617845a0c083a73b30e851
munawerdev <munawer.dev@gmail.com>
2026-06-10 02:14:01 +0500

    add some changes

--- swift_test_project/LoginView.swift
+++ swift_test_project/LoginView.swift
@@ -12,28 +12,42 @@ struct LoginView: View {
     @State var password: String = ""
     
     var body: some View {
-        VStack {
-            Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.primary)
+        ScrollView() {
             
-            // The $ character tells SwiftUI to update the local state variables automatically as the user types
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .autocapitalization(.none) // Handy for email fields
-                .keyboardType(.emailAddress)
             
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Button("Home") {
-                print("Logging in with: \(email)")
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                
+                // The $ character tells SwiftUI to update the local state variables automatically as the user types
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none) // Handy for email fields
+                    .keyboardType(.emailAddress)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Home") {
+                    print("Logging in with: \(email)")
+                }
+                .font(.headline)
+                .buttonStyle(.borderedProminent) // Gives it a nice native tinted background fill
             }
-            .font(.headline)
-            .buttonStyle(.borderedProminent) // Gives it a nice native tinted background fill
+                ForEach(1..<20){
+                    index in 
+                    Text("\(index)")
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding(.bottom, 10)
+                        .padding(.top, 10)
+                }
+            
         }
         .padding(.all)
     }
