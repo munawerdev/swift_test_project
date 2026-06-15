b6c1a3e607f61d13ad191fb78a81989cd8d0ba0c
munawerdev <munawer.dev@gmail.com>
2026-06-16 01:51:00 +0500

    add bottom nav bar

---
+++ swift_test_project/BottomNavBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomNavBar.swift
+//  swift_test_project
+//
+//  Created by Syed Munawer Ali on 15/06/2026.
+//
+
+import SwiftUI
+
+struct BottomNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomNavBar()
+}
